38b76e3adec270fbbbd0b84be2587f16905d21fd
Jochen Klar <jochenklar@gmail.com>
2014-11-06 16:53:12 +0100

    default local settings file added

---
+++ quantify/default.local.py
@@ -0,0 +1,32 @@
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+SECRET_KEY = 'this is a not very secret key'
+
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': '',
+    #     'USER': '',
+    #     'PASSWORD': '',
+    #     'HOST': '',
+    #     'PORT': '',
+    # }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': '',
+    #     'USER': '',
+    #     'PASSWORD': '',
+    #     'HOST': '',
+    #     'PORT': '',
+    # }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': '' # path to database file
+    # }
+}
